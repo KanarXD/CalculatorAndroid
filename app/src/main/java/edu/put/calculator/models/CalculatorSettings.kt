86cf72daf911c41c1ad20e3d697ff1aa5468d882
picio <piciolo@outlook.com>
2022-04-14 10:25:26 +0200

    use kotlin-parcelize

--- app/src/main/java/edu/put/calculator/models/CalculatorSettings.kt
+++ app/src/main/java/edu/put/calculator/models/CalculatorSettings.kt
@@ -1,33 +1,33 @@
 package edu.put.calculator.models
 
-import android.os.Parcel
 import android.os.Parcelable
-import android.os.Parcelable.Creator
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 class CalculatorSettings(val numberPrecision: Int) : Parcelable {
 
-    constructor(parcel: Parcel) : this(
-        parcel.readInt()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(numberPrecision)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Creator<CalculatorSettings> {
-        override fun createFromParcel(parcel: Parcel): CalculatorSettings {
-            return CalculatorSettings(parcel)
-        }
-
-        override fun newArray(size: Int): Array<CalculatorSettings?> {
-            return arrayOfNulls(size)
-        }
-
-    }
+//    constructor(parcel: Parcel) : this(
+//        parcel.readInt()
+//    )
+//
+//    override fun writeToParcel(parcel: Parcel, flags: Int) {
+//        parcel.writeInt(numberPrecision)
+//    }
+//
+//    override fun describeContents(): Int {
+//        return 0
+//    }
+//
+//    companion object CREATOR : Creator<CalculatorSettings> {
+//        override fun createFromParcel(parcel: Parcel): CalculatorSettings {
+//            return CalculatorSettings(parcel)
+//        }
+//
+//        override fun newArray(size: Int): Array<CalculatorSettings?> {
+//            return arrayOfNulls(size)
+//        }
+//
+//    }
 
     override fun toString(): String {
         return "CalculatorSettings(numberPrecision=$numberPrecision)"
